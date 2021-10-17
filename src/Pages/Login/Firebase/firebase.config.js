e2da8c2d429748f1043c71a5a75f46d8abb43449
SanjidaIslamBithi <sanjida333cool@gmail.com>
2021-10-18 00:23:05 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEie9u6R6YZKtFsTN92hGxRy3HUU07gIk",
+  authDomain: "genius-car-mechanics-caeac.firebaseapp.com",
+  projectId: "genius-car-mechanics-caeac",
+  storageBucket: "genius-car-mechanics-caeac.appspot.com",
+  messagingSenderId: "401446446974",
+  appId: "1:401446446974:web:beddb37ccb68a09426352f"
+};
+
+export default firebaseConfig;
